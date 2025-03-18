37f0643257b745e4604455eb98ee041d0414e31d
lorenzo-coppi <coppilorenzo2006@gmail.com>
2025-03-18 23:19:45 +0100

    Add files via upload

---
+++ Hello_Rest/src/main/java/pack/AuthorServices.java
@@ -0,0 +1,25 @@
+package pack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("books/{book_id}/authors")
+public class AuthorServices {
+	
+	public AuthorServices() {}
+	
+	@GET
+	public List<Author> list(@PathParam("{book_id}") long bookId) {
+		Author author = new Author();
+		author.setId(1);
+		author.setName("Joanne");
+		author.setSurname("Rowling");
+		List<Author> authors = new ArrayList<Author>();
+		authors.add(author);
+		return authors;
+	}
+}
